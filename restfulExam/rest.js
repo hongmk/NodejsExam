efd63fec84d15eed749dafc3310f1c6fb57c9ed1
hongmk <isentator@naver.com>
2017-09-30 13:15:20 +0900

    mysql

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -3,12 +3,49 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
-var users = [];
-app.get('/user',function(req,res) {
-	res.send(JSON.stringify(users));
+var mysql = require('mysql');
+var mongodb = require('mongodb');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'test1234',
+  database : 'restful'
 });
+ 
+connection.connect();
+
+app.get('/user',function(req,res) {
+	//res.send(JSON.stringify(users));
+	connection.query('select * from user', function(err, results, fields){
+		if(err){
+			res.send(JSON.stringify(err));
+		} else {
+			res.send(JSON.stringify({length:results.length}) + JSON.stringify(results));
+		}
+	});
+});
+
 app.get('/user/:id',function(req,res){
-	var select_index = -1;
+
+	connection.query('select * from user where id =?', [req.params.id], 
+		function(err, results, fields){
+
+			if(err){
+				res.send(JSON.stringify(err));
+			} else {
+				if(results.length > 0) {
+					//전체를 넘기면 배열로 넘어가므로 1건씩 넘길때는 인덱스를 지정
+					results[0].nextuser = "1"+JSON.stringify(results[1]);
+					res.send(JSON.stringify(results[0]));
+				} else {
+					res.send(JSON.stringify({}));
+				}
+			}
+
+		});
+
+	/*var select_index = -1;
 	for (var i = 0; i < users.length; i++) {
 		var obj = users[i];
 		if (obj.id == Number(req.params.id)) {
@@ -20,20 +57,41 @@ app.get('/user/:id',function(req,res){
 		res.send(JSON.stringify({}));
 	} else {
 		res.send(JSON.stringify(users[select_index]));
-	}
+	}*/
+
 });
 app.post('/user',function(req,res){
-	console.log(req.body.name);
+
+	connection.query('insert into user(name, age) values(?, ?)',[req.body.name,req.body.age],
+		function(err, result) {
+			if(err){
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+	/*console.log(req.body.name);
 	console.log(req.body.age);
 	var name = req.body.name;
 	var age = Number(req.body.age);
 	var obj = {id:users.length+1,name:name,age:age};
 	users.push(obj);
 	res.send(JSON.stringify({result:true,
-		api:'add user info'}));
+		api:'add user info'}));*/
 });
+
 app.put('/user/:id',function(req,res){
-	var select_index = -1;
+	connection.query('update user set name = ? , age = ? where id =?', 
+									[req.body.name,req.body.age,req.params.id],
+		function(err, result){
+			if(err){
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+
+	/*var select_index = -1;
 	for (var i = 0; i < users.length; i++) {
 		var obj = users[i];
 		if (obj.id == Number(req.params.id)) {
@@ -50,9 +108,20 @@ app.put('/user/:id',function(req,res){
 			name:name,age:age};
 		users[select_index] = obj;
 		res.send(JSON.stringify({result:true}));
-	}
+	}*/
 });
+
 app.delete('/user/:id',function(req,res){
+
+	connection.query('delete from user where id=?',
+		[ req.params.id ], function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+	/*
 	var select_index = -1;
 	for (var i = 0; i < users.length; i++) {
 		var obj = users[i];
@@ -66,8 +135,9 @@ app.delete('/user/:id',function(req,res){
 	} else {
 		users.splice(select_index,1);
 		res.send(JSON.stringify({result:true}));
-	}
+	}*/
 });
+
 app.listen(52273,function() {
 	console.log('Server running');
 });

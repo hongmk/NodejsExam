d6808f5c48eacf6705c49eeae8edc75afe0fdfe0
hongmk <isentator@naver.com>
2017-09-30 09:14:03 +0900

    restfulExam (ignore)

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -1,66 +1,74 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended:false}));
 
 var users = [];
-
-app.get('/user', function(req, res){
+app.get('/user',function(req,res) {
 	res.send(JSON.stringify(users));
 });
-
-app.get('/user/:id', function(req, res) {
+app.get('/user/:id',function(req,res){
 	var select_index = -1;
-
-	for(var i=0; i< users.length; i++){
+	for (var i = 0; i < users.length; i++) {
 		var obj = users[i];
-		if(obj.id == Number(req.params.id)) {
+		if (obj.id == Number(req.params.id)) {
 			select_index = i;
 			break;
 		}
 	}
-
-	if(select_index == -1) {
+	if (select_index == -1) {
 		res.send(JSON.stringify({}));
 	} else {
 		res.send(JSON.stringify(users[select_index]));
 	}
-	//res.send(JSON.stringify({api:'get user info', id: req.params.id}));
-
 });
-
-app.post('/user', function(req, res){
+app.post('/user',function(req,res){
+	console.log(req.body.name);
+	console.log(req.body.age);
 	var name = req.body.name;
 	var age = Number(req.body.age);
-	var id = Number(req.body.id);
-
-	var obj = users[id];
-
-	if(obj.name == name && obj.age == age) {
-		res.send(JSON.stringify(obj));
-	} else {
-		res.send("Fail Login");
-	}		
-	
-	
-});
-
-app.put('/user/:id', function(req, res){
-	var name = req.body.name;
-	var age = Number(req.body.age);
-	var obj = {id:users.length+1, name:name, age:age};
+	var obj = {id:users.length+1,name:name,age:age};
 	users.push(obj);
-
-	res.send(JSON.stringify({result:true, api: 'modify user info'}));
-
+	res.send(JSON.stringify({result:true,
+		api:'add user info'}));
+});
+app.put('/user/:id',function(req,res){
+	var select_index = -1;
+	for (var i = 0; i < users.length; i++) {
+		var obj = users[i];
+		if (obj.id == Number(req.params.id)) {
+			select_index = i;
+			break;
+		}
+	}
+	if (select_index == -1) {
+		res.send(JSON.stringify({result:false}));
+	} else {
+		var name = req.body.name;
+		var age = Number(req.body.age);
+		var obj = {id:Number(req.params.id),
+			name:name,age:age};
+		users[select_index] = obj;
+		res.send(JSON.stringify({result:true}));
+	}
+});
+app.delete('/user/:id',function(req,res){
+	var select_index = -1;
+	for (var i = 0; i < users.length; i++) {
+		var obj = users[i];
+		if (obj.id == Number(req.params.id)) {
+			select_index = i;
+			break;
+		}
+	}
+	if (select_index == -1) {
+		res.send(JSON.stringify({result:false}));
+	} else {
+		users.splice(select_index,1);
+		res.send(JSON.stringify({result:true}));
+	}
+});
+app.listen(52273,function() {
+	console.log('Server running');
 });
 
-app.delete('/user/:id', function(req, res){
-	res.send(JSON.stringify({api: 'delete user info', id: req.params.id}));
-});
-
-app.listen(52273, function() {
-	console.log('Server Running');
-});

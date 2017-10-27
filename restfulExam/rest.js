02ef6bd538176729860d1ae160cce937408b9f5e
hongmk <isentator@naver.com>
2017-10-27 17:23:20 +0900

    로그인 및 토큰 읽어오기 추가

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -2,23 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname+'/public'));
 
-//Cross Domain 이슈 대응 (CORS)
+//크로스도메인 이슈 대응 (CORS)
 var cors = require('cors')();
 app.use(cors);
 
 var mysql = require('mysql');
-//********** mysql connection **********
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'test1234',
   database : 'restful'
-});
- 
+}); 
 connection.connect();
-
-//********** mongodb connection **********
+///////////////////////////////////
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/restful';
 var dbObj = null;
@@ -26,39 +24,61 @@ MongoClient.connect(url, function(err, db) {
   console.log("Connected correctly to server");
   dbObj = db;
 });
-
-//********** Mongodb RESTful Logic **********
-app.get('/user/message', function(req, res) {
+///////////////////////////////////
+var multer = require('multer');
+var Storage = multer.diskStorage({
+     destination: function(req, file, callback) {
+         callback(null, "./public/upload_image/");
+     },
+     filename: function(req, file, callback) {
+     		file.uploadedFile = file.fieldname + "_" + 
+     			Date.now() + "_" + file.originalname;
+     		console.log('file.uploadedFile:'+file.uploadedFile);
+         callback(null, file.uploadedFile);
+     }
+ });
+ var upload = multer({
+     storage: Storage
+ }).single("image");
+app.post('/user/picture',function(req, res) {
+	upload(req, res, function(err) {
+		if (err) {
+			res.send(JSON.stringify(err));
+		} else {
+			res.send(JSON.stringify({url:req.file.uploadedFile,
+				description:req.body.description}));
+		}
+	});
+});
+///////////////////////////////////
+app.get('/user/message',function(req,res) {
 	console.log(req.query.sender_id);
-	var messages = dbObj.collection('messages');
 	var condition = {};
-	if(req.query.sender_id != undefined)
+	if (req.query.sender_id != undefined)
 		condition = {sender_id:req.query.sender_id};
-
-	messages.find(condition).toArray(function(err, results){
-		if(err){
+	var messages = dbObj.collection('messages');
+	messages.find(condition)
+		.toArray(function(err, results){
+		if (err) {
 			res.send(JSON.stringify(err));
 		} else {
 			res.send(JSON.stringify(results));
 		}
 	});
 });
-
 var ObjectID = require('mongodb').ObjectID;
-
-app.get('/user/message/:id', function(req, res){
+app.get('/user/message/:id',function(req,res) {
 	var messages = dbObj.collection('messages');
 	messages.findOne(
 		{_id:ObjectID.createFromHexString(req.params.id)},
 		function(err, result){
-			if(err){
+			if (err) {
 				res.send(JSON.stringify(err));
 			} else {
 				res.send(JSON.stringify(result));
 			}
 		});
 });
-
 app.post('/user/message',function(req,res) {
 	console.log(req.body.sender_id);
 	console.log(req.body.reciever_id);
@@ -100,13 +120,12 @@ app.post('/user/message',function(req,res) {
 			}
 		});
 });
-
-app.delete('/user/message/:id', function(req, res){
+app.delete('/user/message/:id',function(req,res) {
 	var messages = dbObj.collection('messages');
-	messages.removeOne(
+	messages.remove(
 		{_id:ObjectID.createFromHexString(req.params.id)},
 		function(err, result){
-			if(err){
+			if (err) {
 				res.send(JSON.stringify(err));
 			} else {
 				res.send(JSON.stringify(result));
@@ -114,56 +133,42 @@ app.delete('/user/message/:id', function(req, res){
 		});
 });
 
-//********** MySQL RESTful Logic **********
-
 app.get('/user',function(req,res) {
-	//res.send(JSON.stringify(users));
-	connection.query('select * from user', function(err, results, fields){
-		if(err){
-			res.send(JSON.stringify(err));
-		} else {
-			res.send(JSON.stringify(results));
-		}
-	});
-});
-
-app.get('/user/:id',function(req,res){
-
-	connection.query('select * from user where id =?', [req.params.id], 
-		function(err, results, fields){
-
-			if(err){
+	connection.query('select * from user', 
+		function(err,results,fields) {
+			if (err) {
 				res.send(JSON.stringify(err));
 			} else {
-				if(results.length > 0) {
-					//전체를 넘기면 배열로 넘어가므로 1건씩 넘길때는 인덱스를 지정
-					//results[0].nextuser = "1"+JSON.stringify(results[1]);
+				res.send(JSON.stringify(results));
+			}
+		});
+});
+app.get('/user/:id',function(req,res){
+	connection.query('select * from user where id=?',
+		[req.params.id], function(err, results, fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				if (results.length > 0) {
 					res.send(JSON.stringify(results[0]));
 				} else {
 					res.send(JSON.stringify({}));
 				}
+				
 			}
-
 		});
-
-	/*var select_index = -1;
-	for (var i = 0; i < users.length; i++) {
-		var obj = users[i];
-		if (obj.id == Number(req.params.id)) {
-			select_index = i;
-			break;
-		}
-	}
-	if (select_index == -1) {
-		res.send(JSON.stringify({}));
-	} else {
-		res.send(JSON.stringify(users[select_index]));
-	}*/
-
 });
-app.post('/user',function(req,res){
 
-	connection.query('insert into user(name, age) values(?, ?)',[req.body.name,req.body.age],
+
+/////////////////////회원가입 및 로그인 구현///////////////////////////
+var crypto  = require('crypto');
+
+app.post('/user',function(req,res){
+	var password = req.body.password;
+	var hash = crypto.createHash('sha256').update(password).digest('base64');
+
+	connection.query('insert into user(user_id, password, name, age) values(?, ?, ?, ?)',
+		[req.body.user_id, hash, req.body.name,req.body.age],
 		function(err, result) {
 			if(err){
 				res.send(JSON.stringify(err));
@@ -181,39 +186,89 @@ app.post('/user',function(req,res){
 		api:'add user info'}));*/
 });
 
+var jwt = require('json-web-token');
+
+app.post('/user/login',function(req,res){
+	var password = req.body.password;
+	var hash = crypto.createHash('sha256').update(password).digest('base64');
+
+	connection.query('select id from user where user_id = ? and password = ?', [req.body.user_id, hash], 
+		function(err, results, fields) {
+			if(err) {
+				res.send(JSON.stringify(err));
+			}else {
+				if(results.length > 0) {
+					//대상있음 -> 로그인성공
+					
+					var cur_date = new Date();
+					var settingAddHeaders = {
+						payload: {
+							"iss":"shinhan",
+							"aud":"mobile",
+							"iat":cur_date.getTime(),
+							"typ":"/online/transactionstatus/v2",
+							"request":{
+								"myTransactionId":req.body.user_id,
+								"merchantTransctionId":hash,
+								"status":"SUCCESS"
+							}
+						},
+						header:{
+							kid:'abcdefghijklmnopqrstuvwxyz1234567890'
+						}
+					};
+					var secret = "SHINHANMOBILETOPSECRET!!!!";
+
+					//위에서 SET한 정보로 토큰생성
+					jwt.encode(secret, settingAddHeaders, 
+						function(err, token){
+							if(err) {
+								res.send(JSON.stringify(err));
+							} else {
+								//토큰이 너무 길기때문에 변화가 있는 부분만 잘라서 사용
+								var tokens = token.split(".");
+
+								//토큰을 user_login 테이블에 저장함
+								connection.query('insert into user_login(user_real_id, token) values(?,?)',
+									[results[0].id, tokens[2]] , 
+									function(err,result) {
+										if(err) {
+											res.send(JSON.stringify(err));
+										} else {
+											res.send(JSON.stringify( {
+												result:true,
+												token:tokens[2],
+												db_result:result
+
+											}));
+										}
+									});
+
+							}
+						});
+
+				} else {
+					//대상없음->로그인실패
+					res.send(JSON.stringify({result:false}));
+				}
+			}
+		});
+});
+///////////////////////////////////////////////////////////
+
 app.put('/user/:id',function(req,res){
-	connection.query('update user set name = ? , age = ? where id =?', 
-									[req.body.name,req.body.age,req.params.id],
-		function(err, result){
-			if(err){
+	connection.query(
+		'update user set name=?,age=? where id=?',
+		[ req.body.name, req.body.age, req.params.id ],
+		function(err, result) {
+			if (err) {
 				res.send(JSON.stringify(err));
 			} else {
 				res.send(JSON.stringify(result));
 			}
-		});
-
-	/*var select_index = -1;
-	for (var i = 0; i < users.length; i++) {
-		var obj = users[i];
-		if (obj.id == Number(req.params.id)) {
-			select_index = i;
-			break;
-		}
-	}
-	if (select_index == -1) {
-		res.send(JSON.stringify({result:false}));
-	} else {
-		var name = req.body.name;
-		var age = Number(req.body.age);
-		var obj = {id:Number(req.params.id),
-			name:name,age:age};
-		users[select_index] = obj;
-		res.send(JSON.stringify({result:true}));
-	}*/
+		})
 });
-
 app.delete('/user/:id',function(req,res){
-
 	connection.query('delete from user where id=?',
 		[ req.params.id ], function(err, result) {
 			if (err) {
@@ -222,24 +277,7 @@ app.delete('/user/:id',function(req,res){
 				res.send(JSON.stringify(result));
 			}
 		});
-	/*
-	var select_index = -1;
-	for (var i = 0; i < users.length; i++) {
-		var obj = users[i];
-		if (obj.id == Number(req.params.id)) {
-			select_index = i;
-			break;
-		}
-	}
-	if (select_index == -1) {
-		res.send(JSON.stringify({result:false}));
-	} else {
-		users.splice(select_index,1);
-		res.send(JSON.stringify({result:true}));
-	}*/
 });
-
 app.listen(52273,function() {
 	console.log('Server running');
 });
-
